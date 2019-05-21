a89e1f57dc5049bd6e586f69986d01aff35a830f
schen <sc30.ucc@gmail.com>
2019-05-21 01:45:08 +0100

    Fix k3: Add HtmlUnit dependency to parse the website which is generated dynamically by js

--- poi-excel-core/src/main/java/ie/sjc/poi/JsoupReadWebPage.java
+++ poi-excel-core/src/main/java/ie/sjc/poi/JsoupReadWebPage.java
@@ -1,5 +1,9 @@
 package ie.sjc.poi;
 
+import com.gargoylesoftware.htmlunit.BrowserVersion;
+import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
+import org.apache.commons.logging.LogFactory;
 import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -8,6 +12,7 @@ import org.jsoup.select.Elements;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.logging.Level;
 
 public class JsoupReadWebPage {
     // TODO: create two fields in the SwingGUI to represent these cssQueries
@@ -39,8 +44,12 @@ public class JsoupReadWebPage {
         //System.out.println(jsoupReadWebPage.requestGetStatus("https://xgx.sooxie.com/96919.aspx"));
         //System.out.println(jsoupReadWebPage.getItemStatus("https://xgx.sooxie.com/96919.aspx"));
 
-        System.out.println(jsoupReadWebPage.requestGetStatus("http://www.k3.cn/p/iiuueiuafod.html?_page=1&_cat=new&_pos=96&_type=img?form=k3_detail"));
-        System.out.println(jsoupReadWebPage.getItemStatus("http://www.k3.cn/p/iiuueiuafod.html?_page=1&_cat=new&_pos=96&_type=img?form=k3_detail"));
+        //System.out.println(jsoupReadWebPage.requestGetStatus("http://www.k3.cn/p/iiuueiuafod.html?_page=1&_cat=new&_pos=96&_type=img?form=k3_detail"));
+        //System.out.println(jsoupReadWebPage.getItemStatus("http://www.k3.cn/p/iiuueiuafod.html?_page=1&_cat=new&_pos=96&_type=img?form=k3_detail"));
+
+        // 测试已下架
+        System.out.println(jsoupReadWebPage.requestGetStatus("http://www.k3.cn/p/iiaaaoeaieb.html?_page=5&_cat=new&_pos=17&_type=img?form=k3_detail"));
+        System.out.println(jsoupReadWebPage.getItemStatus("http://www.k3.cn/p/iiaaaoeaieb.html?_page=5&_cat=new&_pos=17&_type=img?form=k3_detail"));
     }
 
     public int getStatusCode() {
@@ -111,6 +120,8 @@ public class JsoupReadWebPage {
 
         Elements elements = null;
         if (url.contains("k3.cn")) {
+            // Parse html again by using HtmlUnit
+            doc = parseFromHtmlUnit(url);
             if (code == 200) {
                 elements = doc.select(k3);
                 for (Element element : elements) {
@@ -181,5 +192,32 @@ public class JsoupReadWebPage {
         } catch (Exception e) {
             return false;
         }
+    }
+
+    private Document parseFromHtmlUnit(String url) {
+        // 屏蔽HtmlUnit等系统 log
+        LogFactory.getFactory().setAttribute("org.apache.commons.logging.Log","org.apache.commons.logging.impl.NoOpLog");
+        java.util.logging.Logger.getLogger("com.gargoylesoftware").setLevel(Level.OFF);
+        java.util.logging.Logger.getLogger("org.apache.http.client").setLevel(Level.OFF);
+
+
+        // HtmlUnit 模拟浏览器
+        WebClient webClient = new WebClient(BrowserVersion.CHROME);
+        webClient.getOptions().setJavaScriptEnabled(true);              // 启用JS解释器，默认为true
+        webClient.getOptions().setCssEnabled(false);                    // 禁用css支持
+        webClient.getOptions().setThrowExceptionOnScriptError(false);   // js运行错误时，是否抛出异常
+        webClient.getOptions().setThrowExceptionOnFailingStatusCode(false);
+        webClient.getOptions().setTimeout(5 * 1000);                   // 设置连接超时时间
+        HtmlPage page = null;
+        try {
+            page = webClient.getPage(url);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        webClient.waitForBackgroundJavaScript(5 * 1000);               // 等待js后台执行5秒， Exception thrown anyway
+
+        String pageAsXml = page.asXml();
+        Document document = Jsoup.parse(pageAsXml);
+        return document;
     }
 }
